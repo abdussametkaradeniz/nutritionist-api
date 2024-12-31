89a3e6c9ecc5048dd9ab59e6c7ad23d94b2f231a
abdussamet karadeniz <abdussametgosukaradeniz@gmail.com>
2024-12-31 12:45:16 +0300

    Revert "js dosyaları"
    This reverts commit 51c49aca43daa1e8368992ca6fb4aded8fb8098d.

--- src/logger/loggerdeneme.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = log;
-function log(req, res, next) {
-    console.log("logging");
-    next();
-}
--- src/types/appointments/generalAppointmentType.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AppointmentStatus = void 0;
-var AppointmentStatus;
-(function (AppointmentStatus) {
-    AppointmentStatus["PENDING"] = "PENDING";
-    AppointmentStatus["APPROVED"] = "APPROVED";
-    AppointmentStatus["CANCELLED"] = "CANCELLED";
-})(AppointmentStatus || (exports.AppointmentStatus = AppointmentStatus = {}));
